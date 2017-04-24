2573d00afd6d0394c55afcfccf264496147cdf8a
Ponkapa <ball2ball2ball@hotmail.com>
2017-04-24 15:59:40 -0500

    Baseline code from bignerdranch's cake bake app

--- index.js
+++ index.js
@@ -1,44 +1,103 @@
 'use strict';
 module.change_code = 1;
 var _ = require('lodash');
-var Alexa = require('alexa-app');
-var app = new Alexa.app('airportinfo');
-var FAADataHelper = require('./faa_data_helper');
+var Skill = require('alexa-app');
+var CAKE_BAKER_SESSION_KEY = 'cake_baker';
+var skillService = new Skill.app('cakebaker');
+var CakeBakerHelper = require('./cakebaker_helper');
+var DatabaseHelper = require('./database_helper');
+var databaseHelper = new DatabaseHelper();
 
-app.launch(function(req, res){
-	var prompt = 'For delay information, tell me an Airport code.';
-	res.say(prompt).reprompt(prompt).shouldEndSession(false);
+skillService.pre = function(request, response, type) {
+  databaseHelper.createCakeBakerTable();
+}
 
-});
+var getCakeBakerHelper = function(cakeBakerHelperData) {
+  if (cakeBakerHelperData === undefined) {
+    cakeBakerHelperData = {};
+  }
+  return new CakeBakerHelper(cakeBakerHelperData);
+};
 
-app.intent('airportinfo', {
-	'slots': {
-		'AIRPORTCODE': 'FAACODES'
-	},
-	'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
-	},
-	function(req, res) {
-		//get the slot
-		var airportCode = req.slot('AIRPORTCODE');
-		var reprompt = 'Tell me an airport code to get delay information.';
-		if (_.isEmpty(airportCode)){
-			var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
-			res.say(prompt).reprompt(reprompt).shouldEndSession(false);
-			return true;
-		} else {
-			var faaHelper = new FAADataHelper();
-			faaHelper.requestAirportStatus(airportCode).then(function(airportStatus){
-				console.log(airportStatus);
-				res.say(faaHelper.formatAirportStatus(airportStatus)).send();
-			}).catch(function(err){
-				console.log(err.statusCode);
-				var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
-				res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
-			});
-			return false;
-		}
-	}
+var getCakeBakerHelperFromRequest = function(request) {
+  var cakeBakerHelperData = request.session(CAKE_BAKER_SESSION_KEY);
+  return getCakeBakerHelper(cakeBakerHelperData);
+};
+
+var cakeBakerIntentFunction = function(cakeBakerHelper, request, response) {
+  console.log(cakeBakerHelper);
+  if (cakeBakerHelper.completed()) {
+    response.say('Congratulations! Your cake is complete!');
+    response.shouldEndSession(true);
+  } else {
+    response.say(cakeBakerHelper.getPrompt());
+    response.reprompt("I didnt hear you. " + cakeBakerHelper.getPrompt());
+    response.shouldEndSession(false);
+  }
+  response.session(CAKE_BAKER_SESSION_KEY, cakeBakerHelper);
+  response.send();
+};
+
+skillService.intent('advanceStepIntent', {
+    'utterances': ['{next|advance|continue}']
+  },
+  function(request, response) {
+    var cakeBakerHelper = getCakeBakerHelperFromRequest(request);
+    cakeBakerHelper.currentStep++;
+    saveCake(cakeBakerHelper, request);
+    cakeBakerIntentFunction(cakeBakerHelper, request, response);
+  }
 );
 
-console.log(app.utterances().replace(/\{\-\|/g, '{'));
-module.exports = app;
+skillService.launch(function(request, response) {
+  var prompt = 'Welcome to Cakebaker! To start baking, say bake a cake';
+  response.say(prompt).shouldEndSession(false);
+});
+
+skillService.intent('cakeBakeIntent', {
+    'utterances': ['{new|start|create|begin|build} {|a|the} cake']
+  },
+  function(request, response) {
+    var cakeBakerHelper = new CakeBakerHelper({});
+    cakeBakerIntentFunction(cakeBakerHelper, request, response);
+  }
+);
+
+skillService.intent('saveCakeIntent', {
+    'utterances': ['{save} {|a|the|my} cake']
+  },
+  function(request, response){
+    var cakeBakerHelper = getCakeBakerHelperFromRequest(request);
+    saveCake(cakeBakerHelper, request);
+    response.say('Your cake progress has been saved!');
+    response.shouldEndSession(true).send();
+    return false;  
+  }
+);
+
+skillService.intent('loadCakeIntent', {
+  'utterances': ['{load|resume} {|a|the} {|last} cake']
+  },
+  function(request, response){
+    var userId = request.userId;
+    databaseHelper.readCakeBakerData(userId).then(function(result){
+      return (result === undefined ? {} :
+        JSON.parse(result['data']));
+    }).then(function(loadedCakeBakerData){
+      var cakeBakerHelper = new CakeBakerHelper(loadedCakeBakerData);
+      return cakeBakerIntentFunction(cakeBakerHelper, request, response);
+    });
+    return false;
+  }
+)
+
+var saveCake = function(cakeBakerHelper, request){
+  var userId = request.userId;
+  databaseHelper.storeCakeBakerData(userId, JSON.stringify(cakeBakerHelper)).then(function(result){
+    return result;
+  }).catch(function(error){
+    console.log(error);
+  });
+};
+
+module.exports = skillService;
